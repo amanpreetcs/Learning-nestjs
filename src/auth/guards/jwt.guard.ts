14a54489fa124dacb563c349b922e067dc8e7abf
amanpreetcs <amanpreet@crownstack.com>
2025-03-04 14:20:37 +0530

    d

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -19,6 +19,7 @@ export class JwtGuard implements CanActivate {
     }
 
     try {
+      //
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
       });
